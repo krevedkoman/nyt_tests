80ad916a3b66b80772fc90f5e87c2b6fe7c6790d
krevedkoman <ilya@makashov.kz>
2021-10-29 22:35:33 -0400

    Commit first build, add minor changes

--- src/test/java/pages/ApisPage.java
+++ src/test/java/pages/ApisPage.java
@@ -7,14 +7,17 @@ public class ApisPage {
     private String nyt_apis_page_search_value = "Archive API";
 
     public String getNyt_apis_page_search_selector() {
+
         return nyt_apis_page_search_selector;
     }
 
     public String getNyt_apis_page_search_result_selector() {
+
         return nyt_apis_page_search_result_selector;
     }
 
     public String getNyt_apis_page_search_value() {
+
         return nyt_apis_page_search_value;
     }
 }
--- src/test/java/pages/ArchProdPage.java
+++ src/test/java/pages/ArchProdPage.java
@@ -6,10 +6,12 @@ public class ArchProdPage {
     private String getNyt_api_archive_product_looking_value = "https://api.nytimes.com/svc/archive/v1/2019/1.json?api-key=yourkey\n";
 
     public String getNyt_api_archive_product_looking_selector() {
+
         return nyt_api_archive_product_looking_selector;
     }
 
     public String getGetNyt_api_archive_product_looking_value() {
+
         return getNyt_api_archive_product_looking_value;
     }
 }
